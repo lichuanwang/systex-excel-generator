e644e410927d1d814b2a9a38e08091a7d6a9c2c9
Oscar Wang <wanglichuan127@gmail.com>
2024-11-05 10:05:14 +0800

    add getExcelSheet function to ExcelFile, maxColPerRow setter and getter, service

--- src/main/java/com/systex/excelgenerator/excel/ExcelFile.java
+++ src/main/java/com/systex/excelgenerator/excel/ExcelFile.java
@@ -6,24 +6,33 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ExcelFile {
     private final XSSFWorkbook workbook;
 
-    private List<ExcelSheet> sheets;
+//    private List<ExcelSheet> sheets;
 
+    private Map<String, ExcelSheet> sheetMap;
 
     public ExcelFile() {
 
         this.workbook = new XSSFWorkbook();
-        this.sheets = new ArrayList<>();
+        this.sheetMap = new HashMap<>();
     }
 
     // Method to add a new sheet
     public ExcelSheet createSheet(String sheetName) {
         ExcelSheet excelSheet = new ExcelSheet(workbook, sheetName, 10);
-        sheets.add(excelSheet);
+        sheetMap.put(sheetName, excelSheet);
+        return excelSheet;
+    }
+
+    public ExcelSheet getExelSheet(String sheetName) {
+        sheetName = sheetName.trim();
+        ExcelSheet excelSheet = sheetMap.get(sheetName);
         return excelSheet;
     }
 
